9f8c858cd91ba7d808922402e84f48985dc0a7ac
Guto Oliveira <luizaugusto.contato@gmail.com>
2013-11-15 18:00:37 -0200

    adicionando arquivos de front do antigo repositorio canecaria.

---
+++ WebContent/config.rb
@@ -0,0 +1,13 @@
+# Paths configuration
+css_dir = "resources/css"
+sass_dir = "resources/sass"
+images_dir = "resources/images"
+javascripts_dir = "resources/js"
+fonts_dir = "resources/fonts"
+
+# Display the original location (SCSS file and line) of the selector
+# Useful for debugging
+line_comments = true
+
+# SCSS and not SASS
+preferred_syntax = :scss

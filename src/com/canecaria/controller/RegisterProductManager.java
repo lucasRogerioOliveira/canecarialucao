2ddac82c897a33f1d360ed20addee270ad28404d
Bruna Dias <brunacosta63@gmail.com>
2013-12-01 20:32:16 -0200

    Controller de Product

---
+++ src/com/canecaria/controller/RegisterProductManager.java
@@ -0,0 +1,12 @@
+package com.canecaria.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class RegisterProductManager {
+	
+//	private Product product;
+
+}
